f903191a59cf704054bc925b2392e3c7bd6d2e42
Carlitos73 <carl.fuchs@icloud.com>
2021-06-07 16:24:00 +0200

    Flexible start up message with fading

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -14,7 +14,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration.zero, () => alertStartUp());
     return new Scaffold(
       body: new Container(
         padding: EdgeInsets.fromLTRB(20, 70, 20, 50),
@@ -32,6 +31,7 @@ class _LoginPageState extends State<LoginPage> {
               'images/annecyGoTitle.png',
               fit: BoxFit.fitWidth,
             ),
+            _buildMessageStartUp(),
             _buildTextFields(),
             _buildButtons(),
           ],
@@ -120,33 +120,21 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Future<void> alertStartUp() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Bienvenue dans Annecy Go'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text(
-                    "Pour commencer à utiliser l'application. Entrez un nom d'Utilisateur et commencez à accumuler des récompenses en explorant les monuments d'Annecy ! "),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Ok'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+  Widget _buildMessageStartUp() {
+    return new Flexible(
+      child: Container(
+          child: Text(
+              "Pour commencer à utiliser l'application. Entrez un nom d'Utilisateur et commencez à accumuler des récompenses en explorant les monuments d'Annecy seul ou à plusieurs ! ",
+              overflow: TextOverflow.fade,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.black54,
+                fontSize: 40,
+              ))
+      ),
     );
   }
+
 
   Future<void> alertResult(String result) async {
     return showDialog<void>(
