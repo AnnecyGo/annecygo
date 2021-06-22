98b526163ed405b00781d88dbdc14fcf9f3d58a5
leaPortier74 <lea.portier@hotmail.fr>
2021-06-22 11:26:14 +0200

    fin first page ok

--- lib/PagesExplication/firstPage.dart
+++ lib/PagesExplication/firstPage.dart
@@ -1,62 +1,119 @@
+import 'package:annecygo/Login/login.dart';
 import 'package:flutter/material.dart';
-import '../Settings/settings.dart';
-
-
 
 class FirstPage extends StatefulWidget {
+
   @override
   _FirstPageState createState() => _FirstPageState();
 }
 
 class _FirstPageState extends State<FirstPage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Container(
-        padding: EdgeInsets.fromLTRB(20, 70, 20, 50),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            alignment: AlignmentDirectional(0.0, -3.0),
-            image: AssetImage("images/background.png"),
-            fit: BoxFit.fitWidth,
-          ),
-        ),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Image.asset(
-              'images/annecyGoTitle.png',
-              fit: BoxFit.fitWidth,
-            ),
-            _buildMessageStartUp(),
-          ],
-        ),
+    return new WillPopScope(
+      onWillPop: () async => false,
+      child: new Scaffold(
+        body: _contentWidget(),
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.settings),
-          onPressed: () =>
-          {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Settings()),
-            )
-          }),
     );
   }
 
-
-  Widget _buildMessageStartUp() {
-    return new Flexible(
-      child: Container(
-          child: Text(
-              "Pour commencer à utiliser l'application. Entrez un nom d'Utilisateur et commencez à accumuler des récompenses en explorant les monuments d'Annecy seul ou à plusieurs ! ",
-              overflow: TextOverflow.fade,
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                color: Colors.black54,
-                fontSize: 40,
-              ))
+  _contentWidget() {
+    return new Scaffold(
+      backgroundColor: Colors.redAccent,
+      body: new Center(
+        child: new Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [
+                0.5,
+                0.5,
+              ],
+              colors: [
+                Colors.red,
+                Colors.redAccent,
+              ],
+            ),
+          ),
+          margin: const EdgeInsets.only(bottom: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Text("Bienvenue sur\nAnnecy GO !",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                    )),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Text(
+                    "Pour commencer à utiliser l'application,\nentrez un nom d'Utilisateur et commencez à accumuler des récompenses en explorant les monuments d'Annecy seul ou à plusieurs ! ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                    )
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(20.0),
+                child: ButtonTheme(
+                  minWidth: 200.0,
+                  child: new ElevatedButton(
+                    style: ButtonStyle(
+                        padding: MaterialStateProperty.all(
+                            EdgeInsets.fromLTRB(20, 10, 20, 10)),
+                        minimumSize: MaterialStateProperty.all(Size(250.0, 20.0)),
+                        backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              side: BorderSide(color: Colors.white),
+                            )
+                        )
+                    ),
+                    child: new Text(
+                      'Commencer',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black54,
+                        fontSize: 40,
+                      ),
+                    ),
+                    onPressed: _playPressed,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+  void _playPressed() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
     );
   }
 }
+
+
